84b31b86a02e1c2a9c50a8dc2329a3844b88d465
Shubham Kumar Singh <shubham2082003@gmail.com>
2026-02-09 11:44:48 +0530

    Add files via upload

---
+++ file3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x,y,z;
+    printf("I am in G2G section of Btech-1st year\n");
+    printf("Enter the value of x and y- %d%d",x,y);
+    scanf("%d%d", &x,&y);
+    z=x+y;
+    printf("The sum of x and y is %d",z);
+    return 0;
+}
