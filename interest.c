33c8f4a63b5dbfc50f83d493dc5e93ea29b65a0b
shubkr1 <shubham2082003@gmail.com>
2026-02-09 11:24:12 +0530

    Add files via upload

---
+++ interest.c
@@ -0,0 +1,26 @@
+#include <math.h>
+
+int main() {
+    float p, r, t;
+    float si, ci, amount;
+
+    printf("Enter Principal: ");
+    scanf("%f", &p);
+
+    printf("Enter Rate of Interest: ");
+    scanf("%f", &r);
+
+    printf("Enter Time (in years): ");
+    scanf("%f", &t);
+
+    // Simple Interest
+    si = (p * r * t) / 100;
+      // Compound Interest
+    amount = p * pow((1 + r / 100), t);
+    ci = amount - p;
+
+    printf("\nSimple Interest = %.2f", si);
+    printf("\nCompound Interest = %.2f", ci);
+
+    return 0;
+}
