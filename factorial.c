4ca38a970527f894e1e720faedd3ac9354a02636
Shubham Kumar Singh <shubham2082003@gmail.com>
2026-02-23 11:27:16 +0530

    Add files via upload

--- factorial.c
+++ factorial.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int num;
+    int a;
     unsigned long long factorial = 1;
 
     printf("Enter a positive integer: ");
-    scanf("%d", &num);
+    scanf("%d", &a);
 
-    if (num < 0) {
+    if (a < 0) {
         printf("Factorial is not defined for negative numbers.\n");
     } else {
-        for (int i = 1; i <= num; i++) {
+        for (int i = 1; i <= a; i++) {
             factorial *= i;
         }
-        printf("Factorial of %d = %llu\n", num, factorial);
+        printf("Factorial of %d = %llu\n", a, factorial);
     }
 
     return 0;
