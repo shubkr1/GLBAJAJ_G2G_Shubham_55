1edd93c72acfe369164c2bc992af5005805c4995
Shubham Kumar Singh <shubham2082003@gmail.com>
2026-02-23 10:59:07 +0530

    Add files via upload

---
+++ sumofno.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter a number:");
+    scanf ("%d", &a);
+    if (a>0){
+        printf("The number is positive.\n");
+    } 
+    else if (a<0) {
+        printf("The number is negative.\n");
+    }
+    else{ printf("The number is zero.\n");
+    }
+    return 0;
+}
